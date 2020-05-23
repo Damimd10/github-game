62dc0e009466ef7cdf9de54ab1d1336af9b9135b
Damian <damianmdst@gmail.com>
2020-05-23 18:46:25 -0300

    Add call to API and create simple UI to display achievements and stats

--- src/screens/Dashboard/DashboardLayout.tsx
+++ src/screens/Dashboard/DashboardLayout.tsx
@@ -1,15 +1,44 @@
-import React, { FC } from 'react'
-import { useAuth0 } from '../../utils'
+import React, { FC, useEffect, useState } from 'react';
+import { useAuth0 } from '../../utils';
+
+import Achievement from './components/Achievement';
+import Stat from './components/Stat';
 
 const DashboardLayout: FC = () => {
-	const { user, logout } = useAuth0()
+	const [achievements, setAchievements] = useState([]);
+	const { user, logout } = useAuth0();
+
+	useEffect(() => {
+		fetch('http://localhost:4000/api/getUser/damimd10')
+			.then(response => response.json())
+			.then(response => setAchievements(response.achievements_unlocked));
+	}, []);
+
+	const score = achievements.reduce(
+		(accumulator, { score }) => accumulator + score,
+		0
+	);
 
 	return (
-		<div>
-			{JSON.stringify(user)}
-			<button onClick={logout}>LOGOUT</button>
-		</div>
-	)
-}
+		<section className="m-2 flex justify-content-center align-items-center">
+			<section className="w-full text-gray-700 body-font">
+				<div className="m-2">
+					<div className="container px-2 py-2 mx-auto">
+						<div className="flex flex-wrap text-center">
+							<Stat label="score" value={score} />
+							<Stat label="unlocked achievements" value={achievements.length} />
+							<Stat label="locked achievements" value={0} />
+						</div>
+					</div>
+				</div>
+				<div className="w-full flex flex-wrap m-2">
+					{achievements.map(achievement => (
+						<Achievement key={JSON.stringify(achievement)} {...achievement} />
+					))}
+				</div>
+			</section>
+		</section>
+	);
+};
 
-export default DashboardLayout
+export default DashboardLayout;
