487c9a145c79714bba4dd905d2ab19d24bb5c8f1
Damian <damianmdst@gmail.com>
2020-06-02 20:22:35 -0300

    Create progress bar component

---
+++ src/screens/Dashboard/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+
+type ProgressBarProps = {
+	color: string;
+	progress: number;
+};
+
+const ProgressBar: FC<ProgressBarProps> = ({ color, progress }) => {
+	console.log('HERE', progress);
+	return (
+		<div className="h-4 shadow rounded-lg progress-bg">
+			<div
+				className={`h-4 ${color}  rounded-lg text-xs leading-none py-1 text-center text-white`}
+				style={{ width: `${progress}%` }}
+			/>
+		</div>
+	);
+};
+
+export default ProgressBar;
