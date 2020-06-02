8e7f83d96693658e64066364ed13752f44b2943c
Damian <damianmdst@gmail.com>
2020-06-02 20:26:33 -0300

    Refactor achievement component

--- src/screens/Dashboard/components/Achievement/Achievement.tsx
+++ src/screens/Dashboard/components/Achievement/Achievement.tsx
@@ -1,10 +1,24 @@
 import React, { FC } from 'react';
 
 const Achievement: FC<{}> = () => (
-	<div className="flex items-center space-x-2">
+	<>
 		<div className="h-20 w-24 rounded-lg progress-square" />
-		<div className="h-20 w-full rounded-lg progress-square"></div>
-	</div>
+		<div className="h-20 w-full p-2 rounded-lg progress-square">
+			<div className="flex justify-between items-center">
+				<div className="text-lg text-white font-semibold font-custom-common">
+					Achievement One
+				</div>
+				<div className="text-sm text-white font-normal font-custom-common opacity-25">
+					Unlocked 29 May 2020
+				</div>
+			</div>
+
+			<div className="text-xs text-white opacity-50 font-normal font-custom-common">
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum
+			</div>
+		</div>
+	</>
 );
 
 export default Achievement;
--- src/screens/Dashboard/components/Achievements/Achievements.tsx
+++ src/screens/Dashboard/components/Achievements/Achievements.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 
+import Achievement from '../Achievement';
 import ProgressBar from '../ProgressBar';
 
 const Achievements: FC<{}> = () => (
@@ -33,22 +34,7 @@ const Achievements: FC<{}> = () => (
 			</div>
 			<section>
 				<div className="flex items-center space-x-2">
-					<div className="h-20 w-24 rounded-lg progress-square" />
-					<div className="h-20 w-full p-2 rounded-lg progress-square">
-						<div className="flex justify-between items-center">
-							<div className="text-lg text-white font-semibold font-custom-common">
-								Achievement One
-							</div>
-							<div className="text-sm text-white font-normal font-custom-common opacity-25">
-								Unlocked 29 May 2020
-							</div>
-						</div>
-
-						<div className="text-xs text-white opacity-50 font-normal font-custom-common">
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum
-						</div>
-					</div>
+					<Achievement />
 				</div>
 			</section>
 		</section>
