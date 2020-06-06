c49f0018571cac2e5968c55003b9065412314d48
Damian <damianmdst@gmail.com>
2020-06-06 18:27:15 -0300

    Create tab component

---
+++ src/screens/Dashboard/components/Tab/Tab.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import cn from 'classnames';
+
+type TabProps = {
+	active?: boolean;
+	onSelect: (e: unknown) => void;
+	title: string;
+};
+
+const Tab: FC<TabProps> = ({ active, onSelect, title }) => {
+	const linkActiveClass = 'text-xl text-yellow-600 opacity-100';
+	const linkClass =
+		'text-lg text-white font-semibold opacity-50 font-custom-common cursor-pointer hover:opacity-100 hover:text-yellow-500';
+
+	const className = active ? cn(linkClass, linkActiveClass) : linkClass;
+
+	return (
+		<a href="#$" data-id={title} className={className} onClick={onSelect}>
+			{title}
+		</a>
+	);
+};
+
+export default Tab;
